a37a7711a64f867e5ba86d3b112507ae48e8bd17
Muhammed safwan <mesafwan07@gmail.com>
2025-01-02 23:32:39 +0530

    feat: add Sidebar component for navigation

---
+++ client/src/components/core/home/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      <div className="">
+        <h1>Sidebar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
